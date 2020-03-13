8b4a22aaaf7eec8f5033903522d4e339485e8cdd
natanael kristiawan <natanaelkristiawan@hotmail.com>
2020-03-13 19:48:27 +0800

    Banner header

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Prakrin</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+    <link rel="shortcut icon" href="img/icon.png" type="image/png">
+    <link rel="stylesheet" href="css/style.css">
+<body>
+<div class="wrapper">
+     <div class="header">
+        <div class="row">
+            <div class="col-lg-2 header__1">
+                <h1 class="heading-primary heading-primary__1">maxsol</h1>
+            </div>
+            <div class="col-lg-3 header__2"></div>
+            <div class="col-lg-5 header__3">
+                <h1 class="heading-primary">
+                    <span class="heading-primary__2 mt-5">this is</span>
+                    <span class="heading-primary__3">my presentation</span>
+                    <span class="heading-primary__4 my-4">&#124;</span>
+                    <span class="heading-primary__5">ratri</span>
+                    <span class="heading-primary__6">desy</span>
+                    <span class="heading-primary__7">xi rpl 4</span>
+                </h1>
+            </div>
+            <div class="col-lg-2 header__4"></div>
+        </div>
+    </div>
+</div>
+
+<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> -->
+</body>
+</html>
