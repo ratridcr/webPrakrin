b64b90cf417496126b80b8361118b5e87c704305
natanael kristiawan <natanaelkristiawan@hotmail.com>
2020-03-16 18:49:12 +0800

    banner, section-latarBelakang

--- index.php
+++ index.php
@@ -9,25 +9,56 @@
     <link rel="stylesheet" href="css/style.css">
 <body>
 <div class="wrapper">
-     <div class="header">
+     <header class="header">
         <div class="row">
-            <div class="col-lg-2 header__1">
+            <div class="col-md-2 header__1">
                 <h1 class="heading-primary heading-primary__1">maxsol</h1>
+                <span class="welcome welcome__1">welcome</span>
+                <span class="welcome welcome__2">welcome</span>
             </div>
-            <div class="col-lg-3 header__2"></div>
-            <div class="col-lg-5 header__3">
+            <div class="col-md-3 header__2">
+                <div class="header__absen">
+                    <div class="absen">30</div>
+                </div>
+            </div>
+            <div class="col-md-5 header__3">
                 <h1 class="heading-primary">
                     <span class="heading-primary__2 mt-5">this is</span>
                     <span class="heading-primary__3">my presentation</span>
                     <span class="heading-primary__4 my-4">&#124;</span>
                     <span class="heading-primary__5">ratri</span>
                     <span class="heading-primary__6">desy</span>
-                    <span class="heading-primary__7">xi rpl 4</span>
+                    <span class="heading-primary__7 mt-5">xi rpl 4</span>
                 </h1>
             </div>
-            <div class="col-lg-2 header__4"></div>
+            <div class="col-md-2 header__4">
+            <h1 class="heading-primary">
+                    <span class="heading-primary__8">18</span>
+                    <span class="heading-primary__9">18</span>
+                    <span class="heading-primary__10">04</span>
+                    <span class="heading-primary__11">04</span>
+                    <hr class="heading-primary__12">
+                    <span class="heading-primary__13">2020</span>
+                </h1>
+            </div>
         </div>
-    </div>
+    </header>
+
+    <main>
+        <section class="section-latar">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="heading-secondary">latar belakang</h2>
+                    <p class="paragraph pr-5">Dalam kehidupan sekarang, teknologi sangat dibutuhkan untuk membantu pekerjaan manusia dalam kehidupan sehari-harinya.  Oleh karena itu, mempelajari ilmu pengetahuan di bidang teknologi dan informasi sangat penting. Agar memiliki keterampilan serta kemampuan yang akan diterapkan dalam pelaksanaan tugas di lapangan, SMK TI Bali Global sebagai sekolah yang mengkhususkan pendidikan dalam bidang teknologi dan informasi, menambah praktek kerja industri di kelas XI sebagai pelajaran yang wajib bagi semua siswa. Melalui praktek ini para siswa dan siswi dapat memahami serta menghayati pekerjaan sesungguhnya di lapangan.</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="composition">
+                        <img src="img/logo.png" alt="Logo SMK" class="composition__smk mt-5">
+                    </div>
+                </div>
+            </div>  
+        </section>
+    </main>
 </div>
 
 <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> -->
